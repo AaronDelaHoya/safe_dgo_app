28d55d656cebca021447d0149a5ba51d81cac4be
AaronDelaHoya <aaron341251@hotmail.com>
2019-11-15 18:09:12 -0600

    Mejoras

--- lib/Widget/conteiner_info.dart
+++ lib/Widget/conteiner_info.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:safe_dgo_app/costumer/costumer_model.dart';
 
 class ContainerInfo extends StatelessWidget{
+
+  final String nombre;
+  final Icons icon;
+  final
+
+  ContainerInfo({this.nombre,this.icon});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -9,7 +17,10 @@ class ContainerInfo extends StatelessWidget{
         borderRadius: BorderRadius.circular(10.0)
       ),
       child: Column(
-
+        children: <Widget>[
+          Icon(),
+          Text(nombre)
+        ],
       ),
     );
   }
